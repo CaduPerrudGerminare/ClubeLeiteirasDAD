247bf2c46879c2a19dc77117fad14eb861ed03d6
CaduPerrudGerminare <carlos.sousa@germinare.org.br>
2025-04-14 07:12:12 -0300

    Removendo o env

--- Teste-api-livro.js
+++
@@ -1,6 +0,0 @@
-const apiKey = "AIzaSyAYa0pAMWc6pwPwZqKQYhMDifIl6woRDSA";
-const query = "harry potter";
-fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:harry+potter&maxResults=5&key=${apiKey}`)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("Erro:", error));
